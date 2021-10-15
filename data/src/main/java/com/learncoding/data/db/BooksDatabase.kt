54698171583714144b70e3fe7961d1d08ba0d457
Patrick Essiam <pat.essiam701@gmail.com>
2021-10-15 03:21:01 +0000

    Add BooksDatabase class and a converter class

--- data/src/main/java/com/learncoding/data/db/BooksDatabase.kt
+++ data/src/main/java/com/learncoding/data/db/BooksDatabase.kt
@@ -1,4 +1,35 @@
 package com.learncoding.data.db
 
-class BooksDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.learncoding.data.entities.BookEntity
+
+@Database(entities = [BookEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class BooksDatabase : RoomDatabase() {
+    abstract fun bookDao(): BookDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BooksDatabase? = null
+
+        fun getDatabase(appContext: Context): BooksDatabase {
+            val tempInstance =
+                INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    appContext, BooksDatabase::class.java,
+                    BooksDatabase::class.simpleName!!
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
