7c9e629ff8487929891b2ac9e31734ae5c25ca58
Patrick Essiam <pat.essiam701@gmail.com>
2021-10-22 06:29:50 +0000

    Add BooksApi and BooksApiResponse

--- data/src/main/java/com/learncoding/data/api/BooksApi.kt
+++ data/src/main/java/com/learncoding/data/api/BooksApi.kt
@@ -1,4 +1,8 @@
 package com.learncoding.data.api
 
+import retrofit2.Response
+import retrofit2.http.Query
+
 interface BooksApi {
+    suspend fun getBooks(@Query("q") author: String): Response<BooksApiResponse>
 }
--- data/src/main/java/com/learncoding/data/api/BooksApiResponse.kt
+++ data/src/main/java/com/learncoding/data/api/BooksApiResponse.kt
@@ -1,4 +1,20 @@
 package com.learncoding.data.api
 
-class BooksApiResponse {
-}
+import com.squareup.moshi.Json
+
+class BooksApiResponse(val items: List<Item>)
+
+data class Item(
+    @field:Json(name = "id")
+    val id : String,
+    @field:Json(name = "volumeInfo")
+    val volumeInfo: ApiVolumeInfo
+)
+
+data class ApiVolumeInfo(
+    val title: String,
+    val authors: List<String>,
+    val imageLinks: ImageLinks?
+)
+
+data class ImageLinks(val smallThumbnail: String, val thumbnail: String)
